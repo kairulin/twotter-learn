9f2595fc52dec2b1924ac94d42586d2ae6b2c6d3
LinKaiRu <10746041@ntub.edu.tw>
2022-07-12 15:32:27 +0800

    createNewTwoot

---
+++ twotter-learn/README.md
@@ -0,0 +1,26 @@
+# twotter-learn
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Lints and fixes files
+```
+npm run lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
+
+learn it from:(https://www.youtube.com/watch?v=ZqgiuPt5QZo&list=PLAuIKTThUZcKVjyTi0SfZsULJZuoO0OpM&index=12&ab_channel=TheEarthIsSquare)
---
+++ twotter-learn/src/components/UserProfile.vue
@@ -0,0 +1,142 @@
+<template>
+  <div class="user-profile">
+    <div class="user-profile__user-panel">
+      <h1 class="user-profile__username">@{{ user.username }}</h1>
+      <div class="user-profile__admin-badge" v-if="user.isAdmin">Admin</div>
+      <div class="user-profile__follower-count">
+        <strong>Followers:</strong>{{ followers }}
+      </div>
+      <form class="user-profile__create-twoot" @submit.prevent="createNewTwoot">
+        <label for="newTwoot"><strong>New Twoot</strong></label>
+        <textarea id="newTwoot" rows="4" v-model="newTwootContent"/>
+
+        <div class="user-profile__create-twoot-type">
+          <label for="newTwootType"><strong>Type: </strong></label>
+          <select id="newTwootType" v-model="selectedTwootType">
+            <option :value="option.value" v-for="(option,index) in twootTypes" :key="index">
+              {{option.name}}
+            </option>
+          </select>
+        </div>
+
+        <button>
+          Twoot!
+        </button>
+      </form>
+    </div>
+    <div class="user-profile__twoots-wrapper">
+      <TwootItem
+        v-for="twoot in user.twoots"
+        :key="twoot.id"
+        :username="user.username"
+        :twoot="twoot"      
+        @favourite="toggleFavourite"
+        />    
+    </div>
+  </div>
+</template>
+
+<script>
+import TwootItem from "./TwootItem.vue";
+
+export default {
+  name: "UserProfile",
+  components: { TwootItem },
+  props: {},
+  data() {
+    return {
+      newTwootContent:'',
+      selectedTwootType:'instant',
+      twootTypes:[
+        {value:'draft',name:'Draft'},
+        {value:'instant',name:'Instant Twoot'}
+      ],
+      followers: 0,
+      user: {
+        id: 1,
+        username: "_LearnStudent ",
+        firstName: "S",
+        lastName: "T",
+        email: "wl03352521@gmail.com",
+        isAdmin: true,
+        twoots: [
+          { id: 1, content: "Twotter is Amazing!" },
+          { id: 2, content: "I learning it from Youtube!" },
+        ],
+      },
+    };
+  },
+  watch: {
+    followers(newFollowerCount, oldFollowerCount) {
+      if (oldFollowerCount < newFollowerCount)
+        console.log(`${this.user.username} has gained a follower!`);
+    },
+  },
+  computed: {
+    fullName() {
+      return `${this.user.firstName} ${this.user.lastName}`;
+    },
+  },
+  methods: {
+    followUser() {
+      this.followers++;
+    },
+    toggleFavourite(id){
+      console.log(`Favourited Tweet #${id}`)
+    },
+    createNewTwoot(){
+      if(this.newTwootContent && this.selectedTwootType !== 'draft'){
+        this.user.twoots.unshift ({
+          id:this.user.twoots.lenght+1,
+          content:this.newTwootContent
+        })
+        this.newTwootContent='';
+      }
+    }
+  },
+  mounted() {
+    this.followUser();
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.user-profile {
+  display: grid;
+  grid-template-columns: 1fr 3fr;
+  /* width: 100%; */
+  padding: 50px 5%;
+}
+
+.user-profile__user-panel {
+  display: flex;
+  flex-direction: column;
+  margin-right: 50px;
+  padding: 20px;
+  background-color: white;
+  border-radius: 5px;
+  border: 1px solid #dfe3e8;
+}
+
+.user-profile__admin-badge {
+  background: rebeccapurple;
+  color: white;
+  border-radius: 5px;
+  margin-right: auto;
+  padding: 0 10px;
+  font-weight: bold;
+}
+
+.user-profile__create-twoot{
+  display: flex;
+  flex-direction: column;
+
+  border-top:1px solid #dfe3e8;
+  padding-top: 20px;
+}
+
+h1 {
+  margin: 0;
+}
+</style>
